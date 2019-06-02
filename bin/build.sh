7ac94309a0e39a9afb431ec9e832d624732bc3a5
andrzej-tests <andrzej.boguslaw.wisniewski@gmail.com>
2019-06-02 17:08:27 +0200

    prepare build.sh script

---
+++ bin/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+usage () {
+    echo "usage: $0 <sha>"
+    exit 1
+}
+
+if [ "$#" = 0 ]; then
+    set -- "$(curl -sSL 'https://api.github.com/repos/andrzej-tests-1/sentry.app/git/refs/heads/master' | awk -F '"' '$2 == "sha" { print $4 }')"
+    echo "No sha specified, using refs/head/master ($1)"
+fi
+
+[[ "$#" = 1 ]] || usage
+
+sha="$1"
+[[ $sha =~ ^[a-f0-9]{40}$ ]] || usage
+
+set -x
+
+cd ../docker-sentry
+docker build --build-arg SENTRY_BUILD=$sha --rm -t sentry:git git
+docker build --rm -t sentry:git-onbuild git/onbuild
